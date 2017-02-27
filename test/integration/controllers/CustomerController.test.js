d90444f574f41d080ca1580f18492282bbdfad1f
suliat <x00116788@gmail.com>
2017-02-27 09:43:55 +0000

    integration test done

--- test/integration/controllers/CustomerController.test.js
+++ test/integration/controllers/CustomerController.test.js
@@ -1,11 +1,12 @@
 var request = require('supertest');
 const expect = require('chai').expect;
 
-describe('customer model', function(){
+describe('customer Controller', function(){
     it('create new customer', function(done){
         request(sails.hooks.http.app)
         .post('/customer')
         .send({
+            id: 22,
             first_name: "ASHUAHSUHAUS",
             last_name: "AIOEJHIUAEHUIHAEUIAE",
             birth_date: "1970-01-01T00:00:00.000Z"
@@ -19,30 +20,44 @@ describe('customer model', function(){
          }).expect(200);
     }),
     it('update a customer', function(done){
-        request(sails.hooks.http.app).put('/customer/25')
-        .send({
-            
+        request(sails.hooks.http.app)
+        .put('/customer/22')
+        .send({            
                 first_name:"first"
-        }).expect(200,done);
-        // req.end(function(err,res){
-        //     if(err){
-        //         throw err;
-        //     }
-        //     expect(res.status).to.eql('');
-        //     console.log(res.status);
-        //     done();
-        // })
+        }).end(function(err,res){
+             if(err){
+                 done(err);
+             }
+             console.log(res.body, res.statusCode);
+
+             done();
+         }).expect(200);
+    })
+
+    it('delete customer with given id', function(done){
+        request(sails.hooks.http.app)
+        .del('/customer/22')
+        .end(function(err,res){
+             if(err){
+                 done(err);
+             }
+             console.log(res.body, res.statusCode);
+
+             done();
+         }).expect(200);
     })
 
     it('give a joke to customer', function(done){
-        request(sails.hooks.http.app).get('/customer/joker/1')
+        request(sails.hooks.http.app)
+        .get('/customer/joker/1')
         .end(function(err,res){
             if(err){
                 throw err;
             }
             expect(200);
-            console.log(res.body, res.statusCode);
+            console.log(res.text, res.statusCode);
             done();
         })
     })
+
 })
